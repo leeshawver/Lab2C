9f6797a3298e9c9b02958e0441d252f164687483
Lee Shawver <leehshawver@gmail.com>
2022-06-25 21:20:59 -0600

    First commit for Lab2C

---
+++ src/Lab2C.java
@@ -0,0 +1,28 @@
+
+public class Lab2C {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println(i);
+//		}
+//		for (int i = 10; i > -1; i--) {
+//			System.out.println(i);
+//		}
+//		for (int i = 0; i <= 100; i += 2) {
+//			System.out.println(i);
+//		}
+//		for (int i = 0; i <= 100; i++) {
+//			if (i % 2 == 0) {
+//				System.out.println("EVEN");
+//			} else {
+//				System.out.println("ODD");
+//			}
+		int i = 100;
+		while (i > 0) {
+			System.out.println(i % 3);
+			i--;
+		}
+	}
+
+}
